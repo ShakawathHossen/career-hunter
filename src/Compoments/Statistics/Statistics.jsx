7b1a8f9431d6fb4ece0cb7f04db7e159cd46a155
ShakawathHossen <shakawath15-2483@diu.edu.bd>
2023-04-10 18:12:44 +0600

    statitstics updated

--- src/Compoments/Statistics/Statistics.jsx
+++ src/Compoments/Statistics/Statistics.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
+import { ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import './Statistics.css'
 
 const Statistics = () => {
+    const data = [
+        { subject: 'Ass 1', mark: 50, rank: 100 },
+        { subject: 'Ass 2', mark: 55, rank: 133 },
+        { subject: 'Ass 3', mark: 45, rank: 522 },
+        { subject: 'Ass 4', mark: 57, rank: 221 },
+        { subject: 'Ass 5', mark: 60, rank: 322 },
+        { subject: 'Ass 6', mark: 30, rank: 32 },
+        { subject: 'Ass 7', mark: 55, rank: 321 },
+      ];
     return (
         <div>
-            <h1>This is Statics</h1>
+            <div className='md:my-16 my-4'>
+                <h1 className='text-center md:text-3xl text-2xl font-semibold'>Statistics</h1>
+            </div>
+            <div>
+            <div class="grid md:grid-cols-3 grid-cols-1 gap-4 bg-white">
+  <div class="p-4"></div>
+  <div className="hidden md:block p-4 chart-container">
+  <ComposedChart
+    width={600}
+    height={400}
+    data={data}
+    margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+  >
+    <CartesianGrid stroke="#ff1f1" />
+    <XAxis dataKey="subject" />
+    <YAxis />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="mark" barSize={20} fill="#413ea0" />
+    <Line type="monotone" dataKey="mark" stroke="#ff7300" />
+  </ComposedChart>
+</div>
+
+<div className="md:hidden p-4 chart-container">
+  <ComposedChart
+    width={350}
+    height={300}
+    data={data}
+    margin={{ top: 20, right:0, bottom: 20, left: 0 }}
+  >
+    <CartesianGrid stroke="#f1f1f1" />
+    <XAxis dataKey="subject" />
+    <YAxis />
+    <Tooltip />
+    <Bar dataKey="mark" barSize={20} fill="#413ea0" />
+    <Line type="monotone" dataKey="mark" stroke="#ff7300" />
+  </ComposedChart>
+</div>
+
+  {/* on mobile  */}
+  <div class=" p-4"></div>
+</div>
+
+            </div>    
+   
         </div>
+        
     );
 };
 
